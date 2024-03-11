e6e5a2711e0f526f658751fb95895c745f560d18
Priya Rathore <rathorepriya1705@gmail.com>
2024-03-11 17:06:28 +0530

    Code refactoring

--- src/validation/pets.validation.js
+++ src/validation/pets.validation.js
@@ -6,11 +6,11 @@ const addPetSchema = Joi.object({
   weight: Joi.string(),
   breed: Joi.string().required(),
   size: Joi.string(),
-  description: Joi.string().max(50).min(10),
+  description: Joi.string().max(250).min(10),
   size: Joi.string(),
   age: Joi.string().required(),
   sex: Joi.string().valid("Male", "Female").required(),
-  healthInformation: Joi.string().max(20).min(3),
+  healthInformation: Joi.string().max(250).min(3),
   name: Joi.string().min(3).max(20).required().uppercase(),
 });
 
